a9280fec766404d283bc0ee57f4bb089b29d857d
KA-rg <krishjain260905@gmail.com>
2026-07-23 13:15:59 +0530

    admin site fully functional

---
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import  './Sidebar.css'
+import { assets } from '../../assets/assets'
+import { NavLink } from 'react-router-dom'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className="sidebar-options">
+        {/* Automatically applies an "active" class when the current URL matches. */}
+
+        {/* We can customize active styling using className directly now. */}
+        <NavLink to='/add' className="sidebar-option">
+            <img src={assets.add_icon} alt="" />
+            <p>Add Items</p>
+        </NavLink>
+        <NavLink to='/list' className="sidebar-option">
+            <img src={assets.order_icon} alt="" />
+            <p>List Items</p>
+        </NavLink>
+        <NavLink to='/orders' className="sidebar-option">
+            <img src={assets.order_icon} alt="" />
+            <p>Orders</p>
+        </NavLink>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
---
+++ admin/src/pages/Add/Add.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react'
+import './Add.css'
+import { assets, url } from '../../assets/assets';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+const Add = () => {
+
+
+    const [image, setImage] = useState(false);
+    const [data, setData] = useState({
+        name: "",
+        description: "",
+        price: "",
+        category: "Salad"
+    });
+
+    const onSubmitHandler = async (event) => {
+        event.preventDefault();
+
+        if (!image) {
+            toast.error('Image not selected');
+            return null;
+        }
+
+        const formData = new FormData(); //default object to be used directly here, so no model made.
+        formData.append("name", data.name);
+        formData.append("description", data.description);
+        formData.append("price", Number(data.price));
+        formData.append("category", data.category);
+        formData.append("image", image);
+        const response = await axios.post(`${url}/api/food/add`, formData);
+        //Reset:
+        if (response.data.success) {
+            toast.success(response.data.message)
+            setData({
+                name: "",
+                description: "",
+                price: "",
+                category: data.category
+            })
+            setImage(false);
+        }
+        else {
+            toast.error(response.data.message)
+        }
+    }
+
+    const onChangeHandler = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data => ({ ...data, [name]: value }))
+    }
+
+    return (
+        <div className='add'>
+            <form className='flex-col' onSubmit={onSubmitHandler}>
+                <div className='add-img-upload flex-col'>
+                    <p>Upload image</p>
+                    {/*                               take first file if many selected.                                  accept only img file extension */}
+                    <input onChange={(e) => { setImage(e.target.files[0]); e.target.value = '' }} type="file" accept="image/*" id="image" hidden />
+                                                                                                                                {/* gotcha */}
+                    {/* e.target.value = '': This clears the value inside the file input element after a file is selected.If the user selects the same file again, onChange will not fire — because the input’s internal value hasn’t changed.
+                    OnChange only fires if there is a new event.
+                     */}
+                     {/* id is image:- */}
+                    <label htmlFor="image"> 
+                        <img src={!image ? assets.upload_area : URL.createObjectURL(image)} alt="" />
+                                                                    {/* temp blob URL */}
+                    </label>
+                </div>
+                <div className='add-product-name flex-col'>
+                    <p>Product name</p>
+                    <input name='name' onChange={onChangeHandler} value={data.name} type="text" placeholder='Type here' required />
+                </div>
+                <div className='add-product-description flex-col'>
+                    <p>Product description</p>
+                    <textarea name='description' onChange={onChangeHandler} value={data.description} type="text" rows={6} placeholder='Write content here' required />
+                </div>
+                <div className='add-category-price'>
+                    <div className='add-category flex-col'>
+                        <p>Product category</p> 
+                                                    {/* assigns default Salad */}
+                        <select name='category' value={data.category} onChange={onChangeHandler} >
+                            <option value="Salad">Salad</option>
+                            <option value="Rolls">Rolls</option>
+                            <option value="Deserts">Deserts</option>
+                            <option value="Sandwich">Sandwich</option>
+                            <option value="Cake">Cake</option>
+                            <option value="Pure Veg">Pure Veg</option>
+                            <option value="Pasta">Pasta</option>
+                            <option value="Noodles">Noodles</option>
+                        </select>
+                    </div>
+                    <div className='add-price flex-col'>
+                        <p>Product Price</p>
+                        <input type="Number" name='price' onChange={onChangeHandler} value={data.price} placeholder='0' />
+                    </div>
+                </div>
+                <button type='submit' className='add-btn' >ADD</button>
+            </form>
+        </div>
+    )
+}
+
+export default Add
